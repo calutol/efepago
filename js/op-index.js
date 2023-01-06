455d9fb5730389d0bbb8bcf8457f13aaf4856d15
calutol <110926906+calutol@users.noreply.github.com>
2023-01-06 18:59:34 -0400

    modifiacion js

--- js/op-index.js
+++ js/op-index.js
@@ -1,7 +1,7 @@
 nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function(){
-	if (window.pageYOffset > 100) {
+	if (window.pageYOffset > 200) {
 		nav.classList.add('bg-light', 'shadow');
 	} else{
 		nav.classList.remove('bg-light', 'shadow');
